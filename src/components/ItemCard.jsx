0921cb2973eb155594c81d25a90a8d8f537d05bb
zahidbalooshi <zahid.balooshi@gmail.com>
2025-12-29 20:15:15 +0300

    fixed /dashboard link

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -11,9 +11,47 @@ const ItemCard = ({ item }) => {
   }
 
   return (
-      <>
-      <h1>HI dana</h1>
-      </>
+      <div className="left-card">
+      {role === "Car Owner" ? (
+        <>
+          <h3>
+            {item.title} | {item.model}
+          </h3>
+        </>
+      ) : (
+        <>
+          <h3>
+            {item.car.title} | {item.car.model}
+          </h3>
+        </>
+      )}
+      {role === "Garage Owner" ? (
+        <>
+          <p>
+            Request for: <strong>{item.car.carBrand}</strong>
+          </p>
+          <p>Description: {item.description}</p>
+        </>
+      ) : null}
+
+      {role === "Car Owner" ? (
+        <>
+          <button onClick={() => toggleExpand(item.id)}>
+            {expandedId === item.id ? "Close Form" : "Open Service Request"}
+          </button>
+
+
+          {expandedId === item.id && <ServiceRequestForm car={item.title} />}
+        </>
+      ) : (
+        <h1>hi</h1>
+
+
+        // !requestIds.includes(item?._id) && ( //if item id exists, then button won't render
+        //   <button onClick={() => onSubmitRequest(item)}>Show Interest</button>
+        // )
+      )}
+    </div>
   )
 }
 export default ItemCard;
